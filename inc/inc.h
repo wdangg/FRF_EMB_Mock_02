b1da314e10a3935a52758a88c0292a8239cbb60c
wdangg <nthdangg@gmail.com>
2022-11-15 09:39:42 +0700

    [DangNTH3] Self_Update Stupid Code Convention

--- inc/inc.h
+++ inc/inc.h
@@ -7,28 +7,48 @@
 #include "queue.h"
 #include "pit_handle.h"
 
-#define GREEN_LED_PIN 					    (0x5) /* PD5 */
+#define GREEN_LED_PIN 					    (0x5) 
 #define RED_LED_PIN                         (0x1D)
 #define PIT_LOAD_VALUE                      (0xA00000/2)
-#define BUTTON2_PIN 					    (0xC)	/* PC12 */
-#define BUTTON_PIN 						    (0x3)	/* PC3 */
+#define BUTTON2_PIN 					    (0xC)	
+#define BUTTON_PIN 						    (0x3)	
 
-#define QUEUE_SIZE (0x8)
-uint32_t data_arr[QUEUE_SIZE] = {0};
-Queue_Types queue =
-    {
-        0, -1, QUEUE_SIZE, /* Front, Rear, Size */
-        0,
-        &data_arr[0]};
+Object_Type output_led_PE29 = 
+{
+  PORTE,
+  FGPIOE,
+  RED_LED_PIN,
+  OUTPUT
+};
 
-Object_Type ouput_led_PE29 = { PORTE, FGPIOE, RED_LED_PIN, OUTPUT};
+Object_Type ouput_led_PD5 = {
+	PORTD,
+	FGPIOD,
+	GREEN_LED_PIN,
+	OUTPUT
+};
 
-Object_Type ouput_led_PD5 = {PORTD, FGPIOD, GREEN_LED_PIN, OUTPUT};
+PIT_Object_Type pit_object_0 =
+{
+  e_PIT_CHANNEL_0,
+  PIT_LOAD_VALUE,
+  e_TRUE, 
+  e_TRUE,
+  5
+};
 
-PIT_Object_Type pit_object_0 = { e_PIT_CHANNEL_0, PIT_LOAD_VALUE, e_TRUE,  e_TRUE, 5};
+Object_Type input_pin_PC3 = {
+	PORTC,
+	FGPIOC,
+	BUTTON_PIN,
+	INPUT_PULLUP
+};
 
-Object_Type input_pin_PC3 = { PORTC, FGPIOC, BUTTON_PIN, INPUT_PULLUP };
-
-Object_Type input_pin_PC12 = { PORTC, FGPIOC, BUTTON2_PIN, INPUT_PULLUP};
+Object_Type input_pin_PC12 = {
+	PORTC,
+	FGPIOC,
+	BUTTON2_PIN,
+	INPUT_PULLUP
+};
 
 #endif /* __INC_H__ */
--- source/main.c
+++ source/main.c
@@ -1,12 +1,11 @@
 #include "inc.h"
 
-#define RAM_ADDRESS                                     (0x20004000)
-#define BOOT_ADDRESS_BASE                               (0x00000000)
+#define RAM_ADDRESS                                         (0x20004000)
+#define BOOT_ADDRESS_BASE                                   (0x00000000)
 
-#define APP_BASE                                        (0x0003F800)
-#define ADR_BASE_SECTOR_4                               (uint32_t)(0x0003E800UL)
-#define ADR_BASE_SECTOR_2                               (0x0003E800)
-
+#define APP_BASE                                            (0x0003F800)
+#define ADR_BASE_SECTOR_4                                   (uint32_t)(0x0003E800UL)
+#define ADR_BASE_SECTOR_2                                   (0x0003E800)
 
 volatile uint32_t adrres_Sector4 = ADR_BASE_SECTOR_4;
 volatile uint32_t adrres_Sector2 = ADR_BASE_SECTOR_2;
@@ -19,7 +18,6 @@ void CopyFlashFuncToRam(void);
 void Bootloader_jump_AppBilinkLed();
 
 volatile uint32_t address = ADR_BASE_SECTOR_2;
-
 
 volatile uint8_t data;
 volatile uint32_t temp = 0;
@@ -35,17 +33,17 @@ int main()
   /* Disable Global Interrupts */
   __disable_irq();
 
-	/* Config Clock, PORT Function and GPIO Moding */
-	pinMode(&input_pin_PC3);
-	pinMode(&ouput_led_PD5);
-	pinMode(&ouput_led_PE29);
-	pinMode(&input_pin_PC12);
+  /* Config Clock, PORT Function and GPIO Moding */
+  pinMode(&input_pin_PC3);
+  pinMode(&ouput_led_PD5);
+  pinMode(&output_led_PE29);
+  pinMode(&input_pin_PC12);
 
-	/* Attach Interrupts For Pin PC3 */
-	attachExternalInterrupt(PORTC_PORTD_IRQn, &input_pin_PC3, FALLING_EDGE, 0);
+  /* Attach Interrupts For Pin PC3 */
+  attachExternalInterrupt(PORTC_PORTD_IRQn, &input_pin_PC3, FALLING_EDGE, 0);
 
-	/* Attach Interrupts For Pin PC12 */
-	attachExternalInterrupt(PORTC_PORTD_IRQn, &input_pin_PC12, FALLING_EDGE, 0);
+  /* Attach Interrupts For Pin PC12 */
+  attachExternalInterrupt(PORTC_PORTD_IRQn, &input_pin_PC12, FALLING_EDGE, 0);
 
   CopyFlashFuncToRam();
 
@@ -65,14 +63,12 @@ int main()
 
   // Bootloader_jump_AppBilinkLed();
 
-
   while (1)
   {
     if (system_timeout > 20)
     {
       Bootloader_jump_AppBilinkLed();
     }
-
   }
   return 0;
 }
@@ -80,7 +76,7 @@ int main()
 void UART0_DriverIRQHandler()
 {
   if ((UART0->S1 & (UART0_S1_RDRF_MASK)) == UART0_S1_RDRF_MASK)
-  { 
+  {
     system_timeout = 0;
 
     data = CharToDec(UART0->D);
@@ -92,8 +88,7 @@ void UART0_DriverIRQHandler()
       {
         enable_flash = 0;
       }
-      
-    }    
+    }
     else if (data == 'c')
     {
       Erase_Program(address);
@@ -106,7 +101,7 @@ void UART0_DriverIRQHandler()
     {
       if (count == 0)
       {
-        temp |= ((data*16) << 24);
+        temp |= ((data * 16) << 24);
         count++;
       }
       else if (count == 1)
@@ -116,7 +111,7 @@ void UART0_DriverIRQHandler()
       }
       else if (count == 2)
       {
-        temp |= ((data*16) << 16);
+        temp |= ((data * 16) << 16);
         count++;
       }
       else if (count == 3)
@@ -126,7 +121,7 @@ void UART0_DriverIRQHandler()
       }
       else if (count == 4)
       {
-        temp |= ((data*16) << 8);
+        temp |= ((data * 16) << 8);
         count++;
       }
       else if (count == 5)
@@ -136,7 +131,7 @@ void UART0_DriverIRQHandler()
       }
       else if (count == 6)
       {
-        temp |= (data*16);
+        temp |= (data * 16);
         count++;
       }
       else if (count == 7)
@@ -150,9 +145,7 @@ void UART0_DriverIRQHandler()
         address += 0x4;
         count = 0;
       }
-
     }
-
   }
   /* Clear Pending Interrupt */
   NVIC_ClearPendingIRQ(UART0_IRQn);
@@ -160,52 +153,50 @@ void UART0_DriverIRQHandler()
 
 void CopyFlashFuncToRam(void)
 {
-    uint32_t size = 0x100, cntr;
-    uint32_t *pDst = (uint32_t *)RAM_ADDRESS;
-    uint32_t *pSrc = (uint32_t *)BOOT_ADDRESS_BASE;
+  uint32_t size = 0x100, cntr;
+  uint32_t *pDst = (uint32_t *)RAM_ADDRESS;
+  uint32_t *pSrc = (uint32_t *)BOOT_ADDRESS_BASE;
 
-    // func_size = (uint32_t)dummy_endfunc - (uint32_t)func_to_be_copied;
-    for (cntr = 0; cntr < size; cntr++)
-    {
-
-        *pDst++ = *pSrc++;
-    }
-    SCB->VTOR = 0x20004000;
-    // NVIC_SystemReset();
+  // func_size = (uint32_t)dummy_endfunc - (uint32_t)func_to_be_copied;
+  for (cntr = 0; cntr < size; cntr++)
+  {
+    *pDst++ = *pSrc++;
+  }
+  SCB->VTOR = 0x20004000;
+  // NVIC_SystemReset();
 }
 
 void Bootloader_jump_AppBilinkLed()
 {
+  // typedef void (*pFunction)(void);// t?o 1 fn porter
+  __disable_irq();
+  SysTick->CTRL = 0;
+  SysTick->LOAD = 0;
+  SysTick->VAL = 0;
 
-    // typedef void (*pFunction)(void);// t?o 1 fn porter
-    __disable_irq();
-    SysTick->CTRL = 0;
-    SysTick->LOAD = 0;
-    SysTick->VAL = 0;
+  SCB->SHCSR &= ~(SCB_SHCSR_SVCALLPENDED_Msk);
 
-    SCB->SHCSR &= ~(SCB_SHCSR_SVCALLPENDED_Msk);
+  // SCB->VTOR =ADR_BASE_SECTOR_4;
 
-    // SCB->VTOR =ADR_BASE_SECTOR_4;
+  // pFunction l� bi?n ch? d�n h�m jumto
 
-    // pFunction l� bi?n ch? d�n h�m jumto
+  pFunction JumpToApplication = NULL;
+  uint32_t Application_Main_Stack_porter = 0;
+  uint32_t Application_Reset_Handler = 0; // handler
+  /* ----*/
+  /* Initialize the user application Stack Pointe*/
+  Application_Main_Stack_porter = *(volatile uint32_t *)(ADR_BASE_SECTOR_4);
+  __set_MSP(Application_Main_Stack_porter);
 
-    pFunction JumpToApplication = NULL;
-    uint32_t Application_Main_Stack_porter = 0;
-    uint32_t Application_Reset_Handler = 0; // handler
-    /* ----*/
-    /* Initialize the user application Stack Pointe*/
-    Application_Main_Stack_porter = *(volatile uint32_t *)(ADR_BASE_SECTOR_4);
-    __set_MSP(Application_Main_Stack_porter);
+  __DMB();
+  //
+  SCB->VTOR = ADR_BASE_SECTOR_4;
+  __DSB();
+  // SCB->SHCSR &= ~( SCB_SHCSR_SVCALLPENDED_Msk) ;
+  /*jump to app*/
 
-    __DMB();
-    //
-    SCB->VTOR = ADR_BASE_SECTOR_4;
-    __DSB();
-    // SCB->SHCSR &= ~( SCB_SHCSR_SVCALLPENDED_Msk) ;
-    /*jump to app*/
-
-    void (*resethandler)(void) = (void (*)(void))(*(volatile uint32_t *)(ADR_BASE_SECTOR_4 + 0x04U));
-    resethandler();
+  void (*resethandler)(void) = (void (*)(void))(*(volatile uint32_t *)(ADR_BASE_SECTOR_4 + 0x04U));
+  resethandler();
 }
 
 void PIT_IRQHandler_CallBack()
@@ -216,6 +207,6 @@ void PIT_IRQHandler_CallBack()
 
 void PORTC_PORTD_IRQHandler_Callback()
 {
-  GPIO_TogglePin(&ouput_led_PE29);
+  GPIO_TogglePin(&output_led_PE29);
   delay_ms(100);
 }
